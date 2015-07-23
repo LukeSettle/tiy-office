b0e5dac704fc16b04573779da197223524ae0959
Luke Settle <lukewestonsettle@gmail.com>
2015-07-23 00:52:41 -0500

    Picture authors needs to be migrated to be author_id

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,42 @@
+class AuthorsController < ApplicationController
+	def index
+		@authors = Author.all
+	end
+
+  def show
+  	@author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to author_path(@author)
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    if @author.destroy
+      flash[:notice] = "You deleted this post :("
+      redirect_to authors_path
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    redirect_to author_path
+  end
+
+private
+  def author_params
+    params.require(:author).permit(:name)
+  end
+end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,4 @@
 class Picture < ActiveRecord::Base
+	belongs_to :author
+	validates :title, :url, :description, :author, presence: true
 end
