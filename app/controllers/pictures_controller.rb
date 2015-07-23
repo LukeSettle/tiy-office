4feb76a43ded6c79342467b7ab0ce17085ab2072
Luke Settle <lukewestonsettle@gmail.com>
2015-07-22 23:56:31 -0500

    finished CRUD, added simple form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,7 +20,25 @@ class PicturesController < ApplicationController
     @picture.save
     redirect_to picture_path(@picture)
   end
- 
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    if @picture.destroy
+      flash[:notice] = "You deleted this post :("
+      redirect_to pictures_path
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(picture_params)
+    redirect_to picture_path
+  end
+
 private
   def picture_params
     params.require(:picture).permit(:url, :title, :description, :author)
